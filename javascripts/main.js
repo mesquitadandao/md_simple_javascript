61a3d35916356c11dd2eea0d64ed4b57093132fb
mesquitadandao <mesquitadandao@users.noreply.github.com>
2014-01-03 08:03:51 -0300

    add demos.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+;$(function(){
+	var $demoUniq = $("#demo-uniq");
+	
+	$("#apply-uniq").on("click", function(){
+		var text = $demoUniq.val();
+		$demoUniq.val(text.split(',').uniq());
+	})
+
+	var $demoGsub = $("#demo-gsub");
+	var $searchGsub = $("#search-gsub");
+	var $replacementGsub = $("#replacement-gsub");
+
+	$("#apply-gsub").on("click", function(){
+		var text = $demoGsub.val();
+		var search = $searchGsub.val();
+		var replacement = $replacementGsub.val();
+		$demoGsub.val(text.gsub(search, replacement));
+	})
+});
